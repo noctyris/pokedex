c16f693b6dd7c7768abd0aed509f404706f71158
noctyris <onyyyyx@proton.me>
2025-01-21 21:02:00 +0100

    Remove CryptoJS in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from 'react';
 import Papa from 'papaparse';
-import CryptoJS from 'crypto-js';
 
 interface Row {
 	name:		string;
