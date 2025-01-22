e8d5d07a8d1a81ccbc6a94d155a17e5a0ec354bd
noctyris <onyyyyx@proton.me>
2025-01-22 16:57:35 +0100

    Create UI components

---
+++ app/ui/Cross.tsx
@@ -0,0 +1,25 @@
+interface Props {
+	color:	string,
+	size:	number,
+}
+
+export default function Cross(props: Props) {
+	return (
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path 
+			d="M18 6L6 18"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path 
+			d="M6 6L18 18"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+	)
+}
