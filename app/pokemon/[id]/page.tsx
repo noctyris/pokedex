66a33301a591c0dbbe34d0ca3e8fc2f8f4a9cba6
noctyris <onyyyyx@proton.me>
2025-01-28 20:47:08 +0100

    Create organisation around CHE

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useFetchData, getTypesList } from "@/app/data";
+import { useFetchData, getTypesList, Pokemon } from "@/app/data";
 import { usePathname } from "next/navigation";
 
 import Link from "next/link";
@@ -32,22 +32,13 @@ function DataCard(props: DataCardProps) {
 	)
 }
 
-function EvoCard(props: EvoCardProps) {
-	return (
-		<Link href={`/pokemon/${props.id}`} className="flex flex-col items-center bg-cardbg px-2 py-5 border-foreground border-2 rounded-2xl">
-			<p className="text-xs">{props.coe ? props.coe : "Base"}</p>
-			<Image className="aspect-square object-contain" src={props.location} width={100} height={100} alt={`Image de ${props.name}`} />
-			<p className="text-sm">{props.name}</p>
-		</Link>
-	)
-}
 
 function ShowStats(tList: string) {
 	function StatsCard(props: {type: string, value: string}) {
 		return (
 			<div className="flex">
-				<Image src={`/types/${props.type.toLowerCase()}-color.svg`} width={20} height={20} alt={`Type ${props.type.toLowerCase()}`} />
-				<p className="pl-10 py-1">{`${(parseFloat(props.value.slice(1))-1)*100>0 ? "+" : ""}${(parseFloat(props.value.slice(1))-1)*100}% dégats`}</p>
+				<Image src={`/types/${props.type.toLowerCase()}-color.svg`} width={25} height={25} alt={`Type ${props.type.toLowerCase()}`} />
+				<p className="pl-10 py-1">{`${(parseFloat(props.value.slice(1))-1)*100>0 ? "+" : ""}${(parseFloat(props.value.slice(1))-1)*100}% dégats subis`}</p>
 			</div>
 		)
 	}
@@ -62,6 +53,29 @@ function ShowStats(tList: string) {
 	
 }
 
+function groupPokemons(pkmns: Pokemon) {
+	const result: (Pokemon[] | [])[] = []
+	pkmns.forEach((pk) => {
+		const key = parseInt(pk.che.split('_')[1].split('-')[0])-1
+		if (result[key] === undefined) {
+			result.splice(key, 0, [])
+		}
+		result[key].push(pk);
+		result[key].sort((x) => x.che)
+	})
+	return result
+}
+
+function EvoCard(props: EvoCardProps) {
+	return (
+		<Link href={`/pokemon/${props.id}`} className="text-center bg-cardbg border-foreground border-2 rounded-2xl p-4 aspect-square">
+			<p className="text-xs">{props.coe ? props.coe : "Base"}</p>
+			<Image className="mx-auto aspect-square object-contain" src={props.location} width={100} height={100} alt={`Image de ${props.name}`} />
+			<p className="text-sm">{props.name}</p>
+		</Link>
+	)
+}
+
 export default function PokemonPage() {
 	const targetId = usePathname().split('/')[2]
 	const rawData = useFetchData();
@@ -72,13 +86,17 @@ export default function PokemonPage() {
 
 	const pkmn = rawData.filter((pk) => pk.id === targetId)[0];
 	
-	const evoPkmns = rawData.filter((pk) => 
+	const evoPkmns = groupPokemons(rawData.filter((pk) => 
 		pk.che !== "" && 
 		pk.che.split('_')[0] === pkmn.che.split('_')[0]
-	);
+	));
 
-	const evoPkmnsList = evoPkmns.map((pk) => (
-		<EvoCard key={pk.id} location={pk.location} id={pk.id} name={pk.name} coe={pk.coe} />
+	const evoPkmnsList = evoPkmns.map((pkList) => (
+		<div key={nanoid()} className="flex flex-col items-center space-y-4">
+			{pkList.map((pk) => (
+				<EvoCard key={pk.id} location={pk.location} id={pk.id} name={pk.name} coe={pk.coe} />
+			))}
+		</div>
 	))
 		
 	return (
@@ -89,39 +107,42 @@ export default function PokemonPage() {
 					<Cross />
 				</Link>
 			</header>
-			<section className={`flex flex-row justify-around my-5 bg-gradient-to-r from-${pkmn.types[0].toLowerCase()} via-background to-${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()} py-5`}>
-				<Image className={`bg-${pkmn.types[0].toLowerCase()} aspect-square h-fit my-auto p-4 rounded-full`} src={`/types/${pkmn.types[0].toLowerCase()}.svg`} height={100} width={100} alt={`Image du type ${pkmn.types[0]}`} />
-				<Image src={pkmn.location} width={250} height={250} priority alt={`Image fr ${pkmn.name}`} />
-				<Image className={`bg-${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()} aspect-square h-fit my-auto p-4 rounded-full`} src={`/types/${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()}.svg`} height={100} width={100} alt={`Image du type ${pkmn.types[1] !== "" ? pkmn.types[1] : pkmn.types[0]}`} />
-			</section>
-			<section className="px-3 grid gap-[20px] grid-cols-[repeat(auto-fit,minmax(50px,150px))] justify-center">
-				<DataCard title="Numéro" value={pkmn.num} />
-				<DataCard title="Catégorie" value={pkmn.category} />
-				<DataCard title="Génération" value={pkmn.gen.match(/^\d+$/)===null ? pkmn.gen : pkmn.gen + "ᵉ"} />
-				<DataCard title="Poids" value={pkmn.weight ? pkmn.weight + " kg" : "?"} />
-				<DataCard title="Taille" value={pkmn.size ? pkmn.size + " m" : "?" } />
-		 </section>
-		 <section className="py-6 px-5">
-		 	<p className="text-center text-lg pb-2">Statistiques</p>
-		 	<div className="flex flex-col items-center bg-cardbg border-2 w-2/3 pt-2 p-5 mx-auto rounded-3xl">
-		 		<p className="pb-3">Défense</p>
-			 	<div className="flex justify-between w-full">
- 			 		<div className="flex flex-col items-center">
- 			 			<p>Résistance</p>
- 	 				 	{ShowStats(pkmn.resist)}
- 			 		</div>
- 			 		<div className="flex flex-col items-center">
- 			 			<p>Faiblesse</p>
- 					 	{ShowStats(pkmn.weak)}
- 			 		</div>
- 			 	</div>
-		 	</div>
-		 </section>
-			{pkmn.che &&
-			<section>
-				<p className="text-center text-lg pb-3">{"Chaîne d'évolution"}</p>
-				<div className="grid gap-[20px] grid-cols-[repeat(auto-fit,minmax(75px,200px))] justify-center">{evoPkmnsList}</div>
-			</section>}
+			<main className="flex flex-col space-y-5 my-5">
+				<section className={`flex flex-row justify-around bg-gradient-to-r from-${pkmn.types[0].toLowerCase()} via-background to-${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()} py-5`}>
+					<Image className={`bg-${pkmn.types[0].toLowerCase()} aspect-square h-fit my-auto p-4 rounded-full md:scale-100 scale-75`} src={`/types/${pkmn.types[0].toLowerCase()}.svg`} height={100} width={100} alt={`Image du type ${pkmn.types[0]}`} />
+					<Image src={pkmn.location} width={250} height={250} priority alt={`Image fr ${pkmn.name}`} />
+					<Image className={`bg-${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()} aspect-square h-fit my-auto p-4 rounded-full md:scale-100 scale-75`} src={`/types/${pkmn.types[1]!=="" ? pkmn.types[1].toLowerCase() : pkmn.types[0].toLowerCase()}.svg`} height={100} width={100} alt={`Image du type ${pkmn.types[1] !== "" ? pkmn.types[1] : pkmn.types[0]}`} />
+				</section>
+				<section className="px-3 grid gap-[20px] grid-cols-[repeat(auto-fit,minmax(50px,150px))] justify-center">
+					<DataCard title="Numéro" value={pkmn.num} />
+					<DataCard title="Catégorie" value={pkmn.category} />
+					<DataCard title="Génération" value={pkmn.gen.match(/^\d+$/)===null ? pkmn.gen : pkmn.gen + "ᵉ"} />
+					<DataCard title="Poids" value={pkmn.weight ? pkmn.weight + " kg" : "?"} />
+					<DataCard title="Taille" value={pkmn.size ? pkmn.size + " m" : "?" } />
+				</section>
+				{pkmn.che &&
+				<section>
+					<p className="text-center text-lg pb-3">{"Chaîne d'évolution"}</p>
+					<div className="flex md:flex-row flex-col justify-evenly items-center">
+						{evoPkmnsList}
+					</div>
+				</section>}
+				<section>
+					<div className="flex flex-col items-center bg-cardbg border-2 border-foreground w-2/3 p-5 mx-auto rounded-3xl">
+						<div className="flex justify-around w-full md:flex-row flex-col">
+							<div className="flex flex-col items-center space-y-1">
+								<p>Résistance</p>
+								{ShowStats(pkmn.resist)}
+							</div>
+							<hr className="md:invisible visible m-8" />
+							<div className="flex flex-col items-center space-y-1">
+								<p>Faiblesse</p>
+								{ShowStats(pkmn.weak)}
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
 		</>
 	)
 }
