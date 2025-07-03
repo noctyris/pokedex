e34808c559ef47a833bed0dc76544b818b0d1ea1
Noctyris <noctyris@proton.me>
2025-07-03 17:30:11 +0200

    Fix error

--- app/ui/pkpage/ShowWS.tsx
+++ app/ui/pkpage/ShowWS.tsx
@@ -12,7 +12,7 @@ function WS(tList: string) {
 		)
 	}
 
-    const ws = getTypesList().map((type) => tList.toLowerCase().includes(type) && tList.slice(tList.toLowerCase().indexOf(type)).toLowerCase().split(' ').slice(0,2)).filter(item => item)a.sort((a,b) => parseFloat(b[1]) - parseFloat(a[1])).reverse();
+    const ws = getTypesList().map((type) => tList.toLowerCase().includes(type) && tList.slice(tList.toLowerCase().indexOf(type)).toLowerCase().split(' ').slice(0,2)).filter(item => item).sort((a,b) => parseFloat(b[1]) - parseFloat(a[1])).reverse();
     console.log(tList);
     console.log(ws);
 
